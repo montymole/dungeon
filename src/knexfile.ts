cb0417e019aae09652f31e8dee00f9e59be49331
jlof <jussi.lof@geniem.com>
2018-12-06 17:00:44 +0100

    remove unnessesitys

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,46 +1,46 @@
 // Update with your config settings.
 const config = {
   development: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: process.env.DB_HOST || 'localhost',
-      user: 'root',
-      password: '',
-      database: process.env.DATABASE || 'dungeon',
+      host: process.env.DB_HOST || "localhost",
+      user: "root",
+      password: process.env.PASSWORD || "",
+      database: process.env.DATABASE || "dungeon"
     },
     migrations: {
-      directory: './migrations',
-      extensions: ['.ts'],
-      stub: './migrationStub.ts',
+      directory: "./migrations",
+      extensions: [".ts"],
+      stub: "./migrationStub.ts"
     },
     dbManager: {
-      collate: ['fi_FI.UTF-8', 'utf8_swedish_ci'],
-      superUser: 'root',
-      superPassword: '',
-    },
+      collate: ["fi_FI.UTF-8", "utf8_swedish_ci"],
+      superUser: "root",
+      superPassword: process.env.PASSWORD || ""
+    }
   },
   /**
    * @todo Assign a non-root user with a proper password before deploying!
    */
   production: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: process.env.DB_HOST || 'mariadb',
-      user: 'root',
-      password: '',
-      database: process.env.DATABASE || 'dungeon',
+      host: process.env.DB_HOST || "mariadb",
+      user: "root",
+      password: process.env.PASSWORD || "",
+      database: process.env.DATABASE || "dungeon"
     },
     migrations: {
-      directory: './migrations',
-      extensions: ['.ts'],
-      stub: './migrationStub.ts',
+      directory: "./migrations",
+      extensions: [".ts"],
+      stub: "./migrationStub.ts"
     },
     dbManager: {
-      collate: ['fi_FI.UTF-8', 'utf8_swedish_ci'],
-      superUser: 'root',
-      superPassword: '',
-    },
-  },
+      collate: ["fi_FI.UTF-8", "utf8_swedish_ci"],
+      superUser: "root",
+      superPassword: process.env.PASSWORD || ""
+    }
+  }
 };
 module.exports = config;
 export default config;
