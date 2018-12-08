7e33fc36fa0b1a800903c089f23e45c4b5160240
jlof <jussi.lof@geniem.com>
2018-12-08 17:44:43 +0100

    physics bodies

--- src/app/components/Dungeon.ts
+++ src/app/components/Dungeon.ts
@@ -13,8 +13,9 @@ class Dungeon3D extends ThreeObj {
   meshMap: any = {};
   visibleMeshes: any = [];
   materials: any = {};
+  walls: any;
 
-  initGraphics (dungeonMap) {
+  initGraphics(dungeonMap) {
     Object.keys(dungeonMap.tiles).map(k => {
       const tile = dungeonMap.tiles[k];
       let tileMesh;
@@ -55,7 +56,7 @@ class Dungeon3D extends ThreeObj {
     });
   }
 
-  initPhysics (dungeonMap) {
+  initPhysics(dungeonMap) {
     const { world } = this;
     // create physics floor
     const floor = new CANNON.Body({
@@ -65,38 +66,73 @@ class Dungeon3D extends ThreeObj {
     });
     if (world && world.physics) world.physics.addBody(floor);
     // create walls
+    this.walls = [];
     const { rooms, tiles } = dungeonMap;
     rooms.forEach(room => {
-      // create wall box
-      //console.log('ROOM', room.x, room.y, room.w, room.h);
-      const wall = { x1: room.x, y1: room.y, x2: room.x, y2: room.y };
-      let x1 = room.x;
-      let x2 = room.x;
-      for (let x = room.x; x < room.x + room.w; x++) {
-        const k = `x${x}y${room.y}`;
-        const tile = tiles[k];
-        // northwall
-        if (tile.type === TILE_TYPE.WALL) {
-          //is wall
-          x2 = x;
-        } else {
-          //wall ends
-          this.wallBoxHelper(x1, room.y, x2, room.y + 1);
-          x1 = x;
-        }
-      }
-      this.wallBoxHelper(x1, room.y, x2, room.y + 1);
+      // create wall boxes
+      this.horizontalWall(tiles, room, room.y);
+      this.horizontalWall(tiles, room, room.y - 1 + room.h);
+      this.verticalWall(tiles, room, room.x);
+      this.verticalWall(tiles, room, room.x - 1 + room.w);
     });
   }
 
-  wallBoxHelper (x1, y1, x2, y2) {
+  horizontalWall(tiles, room, y) {
+    let x1 = room.x;
+    let x2 = room.x;
+    for (let x = room.x; x < room.x + room.w; x++) {
+      const k = `x${x}y${y}`;
+      const tile = tiles[k];
+      // northwall
+      if (tile.type === TILE_TYPE.WALL) {
+        x2 = x;
+      } else {
+        //wall ends
+        this.wallBox(x1 + 0.5, y - 0.5, x2 + 0.5, y + 0.5);
+        x1 = x;
+      }
+    }
+    if (x1 !== x2) this.wallBox(x1 + 0.5, y - 0.5, x2 + 0.5, y + 0.5);
+  }
+
+  verticalWall(tiles, room, x) {
+    let y1 = room.y + 1;
+    let y2 = room.y + 1;
+    for (let y = room.y + 1; y < room.y + room.h; y++) {
+      const k = `x${x}y${y}`;
+      const tile = tiles[k];
+      // northwall
+      if (tile.type === TILE_TYPE.WALL) {
+        y2 = y;
+      } else {
+        //wall ends
+        this.wallBox(x - 0.5, y1 - 0.5, x + 0.5, y2 - 0.5);
+        y1 = y;
+      }
+    }
+    if (y1 !== y2) this.wallBox(x - 0.5, y1 - 0.5, x + 0.5, y2 - 0.5);
+  }
+
+  wallBox(x1, y1, x2, y2) {
+    const { world } = this;
     const box = new THREE.Box3();
     box.set(new THREE.Vector3(x1, 0, y1), new THREE.Vector3(x2, 3, y2));
     const helper = new THREE.Box3Helper(box, 0xffff00);
-    this.world.scene.add(helper);
+    this.add(helper);
+    const h = 2.5;
+    const w = x2 - x1;
+    const d = y2 - y1;
+    const shape = new CANNON.Box(new CANNON.Vec3(w, h, d));
+    const body = new CANNON.Body({
+      mass: 0,
+      shape,
+      position: new CANNON.Vec3(x1, y2, 0)
+    });
+    if (world && world.physics) world.physics.addBody(body);
+    this.walls.push({ body, helper });
   }
 
-  async init (props) {
+  async init(props) {
     this.glbSrc = "/gitf/test.glb";
     await super.init(props);
     this.seed = props.dungeonMap.seed;
@@ -104,11 +140,15 @@ class Dungeon3D extends ThreeObj {
     this.initPhysics(props.dungeonMap);
   }
 
-  async update (props) {
+  destroy() {
+    //TODO destroy phys bodies from world.physics
+    this.world.scene.remove(this);
+  }
+
+  async update(props) {
     if (props.dungeonMap.seed !== this.seed) {
       await this.init(props);
     }
-
     const { playerFov } = props;
     if (playerFov) {
       const visibleNow = [];
@@ -118,9 +158,7 @@ class Dungeon3D extends ThreeObj {
           this.meshMap[key].visible = true;
         }
       });
-      difference(this.visibleMeshes, visibleNow).forEach(
-        key => (this.meshMap[key].visible = false)
-      );
+      difference(this.visibleMeshes, visibleNow).forEach(key => (this.meshMap[key].visible = false));
       this.visibleMeshes = visibleNow;
     }
   }
